1b54083ce00bf39e8868ef33de781d0f14f5fe14
vandana-123-v <vandana.vijayakumar2004@gmail.com>
2025-11-06 15:51:22 +0530

    Add files via upload

---
+++ pgm21.php
@@ -0,0 +1,10 @@
+<?php
+$a=array("Anjala","albin","benny","angel");
+print_r($a);
+echo("<br>Ascending order:");
+asort($a);
+print_r($a);
+echo("<br>Descending order:");
+arsort($a);
+print_r($a);
+?>
